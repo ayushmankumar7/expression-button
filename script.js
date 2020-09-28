3c75431734f007c31467e510b136447fe8044a9f
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-09-28 21:44:59 +0530

    music added

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:3000')
 
+const button = document.querySelector("#but");
+
 socket.on('chat-message', data => {
     console.log(data)
+})
+
+button.addEventListener("click", e=>{
+    e.preventDefault()
+    console.log("Clicked")
+    var audio = new Audio('music.mp3');
+    audio.play();
 })
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const io = require('socket.io')(3000)
 
 io.on('connection', socket => {
+    console.log("New User")
     socket.emit('chat-message', "Hello World")
 })
 
